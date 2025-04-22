a5e90bdcbdfadbebc78af6a09d9c41be00920d7b
nreos32 <blooalt2@gmail.com>
2025-04-21 22:45:30 -0400

    777

---
+++ server/src/config/db.js
@@ -0,0 +1,3 @@
+// This file forwards the db config to maintain compatibility with different file structures
+// between development and production environments
+module.exports = require("../../config/db");
